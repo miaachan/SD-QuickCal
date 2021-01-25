f4f95bfd368a2f3f7459a213d8e75fd213085aa9
miaachan <kimyeungg@outlook.com>
2021-01-25 19:40:34 +0800

    feat: add Handy conversion guide

--- src/Component/Cheatsheet.tsx
+++ src/Component/Cheatsheet.tsx
@@ -5,7 +5,14 @@ function Cheatsheet() {
     return (
         <ReactMarkdown>
             {`
-## Numbers
+### [Handy conversion guide](https://github.com/donnemartin/system-design-primer/blob/master/solutions/system_design/scaling_aws/README.md#calculate-usage)
+
+- 2.5 million seconds per month
+- 1 request per second = 2.5 million requests per month
+- 40 requests per second = 100 million requests per month
+- 400 requests per second = 1 billion requests per month
+
+###  Numbers
 
 - Latencies:
 - Hashing:
